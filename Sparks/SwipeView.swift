e653c282a7498e7d4eb18d9348c366c339178bf8
Ryan Farley <ryfarley@gmail.com>
2015-04-24 14:12:45 -0500

    SwipeView Constraints

--- Sparks/SwipeView.swift
+++ Sparks/SwipeView.swift
@@ -31,10 +31,17 @@ class SwipeView: UIView {
     private func initialize() {
         self.backgroundColor = UIColor.clearColor()
         addSubview(card)
+        
+        card.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
         setConstraints()
     }
     
-    private func setConstraints() {
+    private func setConstraints(){
         
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0))
     }
 }
