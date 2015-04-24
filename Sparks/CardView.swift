f84c9e320e9ae6deba843a2d5af706aa725446c0
Ryan Farley <ryfarley@gmail.com>
2015-04-24 13:44:58 -0500

    Create a CardView Class

---
+++ Sparks/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Sparks
+//
+//  Created by Ryan Farley on 4/24/15.
+//  Copyright (c) 2015 ryanfarley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CardView: UIView {
+    
+    private let imageView: UIImageView = UIImageView()
+    private let nameLabel: UILabel = UILabel()
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    override init() {
+        super.init()
+        initialize()
+    }
+    
+    private func initialize() {
+        imageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        imageView.backgroundColor = UIColor.redColor()
+        addSubview(imageView)
+        nameLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+        addSubview(nameLabel)
+        backgroundColor = UIColor.whiteColor()
+        layer.borderWidth = 0.5
+        layer.borderColor = UIColor.lightGrayColor().CGColor
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+    }
+}
