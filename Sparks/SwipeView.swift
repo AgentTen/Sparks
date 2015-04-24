5e8880c7ba4b0785345ab0a642ecb6030d63218c
Ryan Farley <ryfarley@gmail.com>
2015-04-24 15:01:37 -0500

    Track Direction

--- Sparks/SwipeView.swift
+++ Sparks/SwipeView.swift
@@ -11,6 +11,12 @@ import UIKit
 
 class SwipeView: UIView {
     
+    enum Direction {
+        case None
+        case Left
+        case Right
+    }
+    
     private let card: CardView = CardView()
     
     private var originalPoint: CGPoint?
@@ -58,6 +64,20 @@ class SwipeView: UIView {
             println("Default triggered for GestureRecognizer")
             break
         }
+    }
+    
+    func swipe(s:Direction) {
+        
+        if s == .None {
+            return
+        }
+        var parentWidth = superview!.frame.size.width
+        if s == .Left {
+            parentWidth *= -1
+        }
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+            self.center.x = self.frame.origin.x + parentWidth
+        })
     }
     
     private func resetViewPositionAndTransformations() {
