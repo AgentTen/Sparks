48d560b3fdf2c30987c012b13d9a3b79283612f7
Ryan Farley <ryfarley@gmail.com>
2015-04-24 14:19:55 -0500

    Create CardsViewController

---
+++ Sparks/CardsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CardsViewController.swift
+//  Sparks
+//
+//  Created by Ryan Farley on 4/24/15.
+//  Copyright (c) 2015 ryanfarley. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    @IBOutlet weak var cardsStackView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
