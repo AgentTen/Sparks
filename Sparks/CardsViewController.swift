135568f3e3d3f6b013c6460ee37821e1f7f788d2
Ryan Farley <ryfarley@gmail.com>
2015-04-26 18:14:50 -0500

    Adding a Card Struct

--- Sparks/CardsViewController.swift
+++ Sparks/CardsViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class CardsViewController: UIViewController, SwipeViewDelegate {
+    
+    struct Card {
+        let cardView: CardView
+        let swipeView: SwipeView
+    }
 
     let frontCardTopMargin: CGFloat = 0
     let backCardTopMargin: CGFloat = 10
@@ -42,6 +47,14 @@ class CardsViewController: UIViewController, SwipeViewDelegate {
     
     private func createCardFrame(topMargin: CGFloat) -> CGRect {
         return CGRectMake(0, topMargin, cardsStackView.frame.width, cardsStackView.frame.height)
+    }
+    
+    private func createCard(topMargin: CGFloat) -> Card {
+        let cardView = CardView()
+        let swipeView = SwipeView(frame: createCardFrame(topMargin))
+        swipeView.delegate = self
+        swipeView.innerView = cardView
+        return Card(cardView: cardView, swipeView: swipeView)
     }
     
     // MARK: SwipeViewDelegate
