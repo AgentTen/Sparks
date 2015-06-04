dd991a3879a9048d68f81f3d8b34d60d5b985b11
Ryan Farley <ryfarley@gmail.com>
2015-06-04 09:27:57 -0500

    Create a ProfileViewController

---
+++ Sparks/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  Sparks
+//
+//  Created by Ryan Farley on 6/4/15.
+//  Copyright (c) 2015 ryanfarley. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
