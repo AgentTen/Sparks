2b293ef949fe4ff229c02f9e9bcd62e293f16e75
Ryan Farley <ryfarley@gmail.com>
2015-06-10 14:12:09 -0500

    Updating navigationitems in the profileviewcontroller

--- Sparks/ProfileViewController.swift
+++ Sparks/ProfileViewController.swift
@@ -14,6 +14,14 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.titleView = UIImageView(image: UIImage(named: "profile-header"))
+        let rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "nav-back-button"), style: UIBarButtonItemStyle.Plain, target: self, action: "goToCards:")
+        navigationItem.setRightBarButtonItem(rightBarButtonItem, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +39,9 @@ class ProfileViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func goToCards(button:UIBarButtonItem) {
+        
     }
 }
