9b05d96cde8255512f7f2c432e0a4903d446eb66
Ryan Farley <ryfarley@gmail.com>
2015-06-11 15:34:28 -0500

    Saving matches

--- Sparks/User.swift
+++ Sparks/User.swift
@@ -59,9 +59,19 @@ func saveSkip(user: User) {
 }
 
 func saveLike(user: User) {
-    let like = PFObject(className: "Action")
-    like.setObject(PFUser.currentUser()!.objectId!, forKey: "byUser")
-    like.setObject(user.id, forKey: "toUser")
-    like.setObject("liked", forKey: "type")
-    like.saveInBackground()
+    
+    PFQuery(className: "Action").whereKey("byUser", equalTo: user.id).whereKey("toUser", equalTo: PFUser.currentUser()!.objectId!).whereKey("type", equalTo: "liked").getFirstObjectInBackgroundWithBlock() {
+        object, error in
+        var matched = false
+        if object != nil {
+            matched = true
+            object?.setObject("matched", forKey: "type")
+            object?.saveInBackground()
+        }
+        let match = PFObject(className: "Action")
+        match.setObject(PFUser.currentUser()!.objectId!, forKey: "byUser")
+        match.setObject(user.id, forKey: "toUser")
+        match.setObject(matched ? "matched" : "liked", forKey: "type")
+        match.saveInBackground()
+    }
 }
